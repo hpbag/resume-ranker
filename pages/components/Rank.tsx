7bf9e5fde854587103cea4a56010daed75e71a55
Harpriya Bagri <harpriyabagri@gmail.com>
2023-06-14 00:29:24 -0700

    new commit for the env vars

--- pages/components/Rank.tsx
+++ pages/components/Rank.tsx
@@ -4,7 +4,6 @@ import { FilePond, FilePondProps } from 'react-filepond';
 import { Button, Container, Stack, Input, Box, Text } from '@chakra-ui/react';
 import 'filepond/dist/filepond.min.css';
 
-//converting any blob into a URL
 async function basicUpload(params) {
   const baseUrl = "https://api.upload.io";
   const path = `/v2/accounts/${params.accountId}/uploads/binary`;

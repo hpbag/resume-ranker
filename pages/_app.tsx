0d23879e9bc3ba58e35a1b700f939134102cd74a
Harpriya Bagri <harpriyabagri@gmail.com>
2023-06-14 00:42:28 -0700

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
   )
 }
 
